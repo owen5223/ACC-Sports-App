58390a2347de5eb77d0319cdc70d5cdb00610ba4
Owen deYoung <deyoungowen@gmail.com>
2019-04-02 20:16:34 -0500

    java scrirpt files for each page

--- public/js/index.js
+++ public/js/index.js
@@ -3,36 +3,36 @@ $(document).ready(function() {
     $(".btn").addClass("animated jello").css("background-color","#6d0d7a")
   })
 
-  $.ajax({
-      url:"https://api.fantasydata.net/v3/cbb/scores/JSON/LeagueHierarchy",
-      beforeSend: function(xhrObj){
-          // Request headers
-          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","345ec2b2ce2e41fe9d552aee5ca3deba");
-      },
-      type: "GET",
-  })
-  .done(function(data) {
-      var accTeams= data[2].Teams; //accTeams is array of objects that contain information about each team
-      var apTeams = accTeams.filter(function(team){
-        return team.ApRank !== null
-      }).sort(function(a,b){
-        return a.ApRank - b.ApRank
-      })
-      var nullTeams = accTeams.filter(function(team){
-        return team.ApRank === null
-      }).sort(function(a,b){
-        return b.Wins - a.Wins
-      })
-      var allTeams = apTeams.concat(nullTeams)
-      for (var i = 0; i < allTeams.length; i++) {
-        var team = document.createTextNode(allTeams[i].School);
-        var el = document.createElement('li');
-        el.appendChild(team);
-        $('#topTeamsOrderedList').append(el);
+  // $.ajax({
+  //     url:"https://api.fantasydata.net/v3/cbb/scores/JSON/LeagueHierarchy",
+  //     beforeSend: function(xhrObj){
+  //         // Request headers
+  //         xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","345ec2b2ce2e41fe9d552aee5ca3deba");
+  //     },
+  //     type: "GET",
+  // })
+  // .done(function(data) {
+  //     var accTeams= data[2].Teams; //accTeams is array of objects that contain information about each team
+  //     var apTeams = accTeams.filter(function(team){
+  //       return team.ApRank !== null
+  //     }).sort(function(a,b){
+  //       return a.ApRank - b.ApRank
+  //     })
+  //     var nullTeams = accTeams.filter(function(team){
+  //       return team.ApRank === null
+  //     }).sort(function(a,b){
+  //       return b.Wins - a.Wins
+  //     })
+  //     var allTeams = apTeams.concat(nullTeams)
+  //     for (var i = 0; i < allTeams.length; i++) {
+  //       var team = document.createTextNode(allTeams[i].School);
+  //       var el = document.createElement('li');
+  //       el.appendChild(team);
+  //       $('#topTeamsOrderedList').append(el);
 
-      }
-  })
-  .fail(function() {
-      console.log("error");
-  });
+  //     }
+  // })
+  // .fail(function() {
+  //     console.log("error");
+  // });
 });
