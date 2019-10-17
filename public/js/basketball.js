4bd9e43a3f85102b977453235616e0d15fd498a2
Owen deYoung <deyoungowen@gmail.com>
2019-10-16 19:06:08 -0500

    wired up player stats request

--- public/js/basketball.js
+++ public/js/basketball.js
@@ -27,10 +27,26 @@ $(document).ready(function() {
         var allTeams = apTeams.concat(nullTeams)
         for (var i = 0; i < allTeams.length; i++) {
          
-          var el = $("<li></li>").append(allTeams[i].School)
+          var el = $("<li></li>").append(allTeams[i].School).data("school",allTeams[i].Key)
           var logo= $("<img />").attr("src",allTeams[i].TeamLogoUrl).css("width", "40px").css("margin-left","10px")
           $(el).append(logo);
+          $(el).on("click", function(e) {
+            $("#playersList").empty().addClass("spinner-grow mt-3 ml-5 ")
+            var school= $(e.currentTarget).data().school
+            $.ajax({
+              url:"https://api.sportsdata.io/v3/cbb/scores/json/Players/"+school,
+              beforeSend: function(xhrObj){
+                  // Request headers
+                  xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","345ec2b2ce2e41fe9d552aee5ca3deba");
+              },
+              type: "GET",
+
           
+            }).done(function(data){
+              console.log(data)
+              $("#playersList").removeClass("spinner-grow mt-3 ml-5")
+            })
+          })
          
           $('#topTeamsOrderedList').append(el);
   
